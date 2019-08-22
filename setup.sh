d6f569898f7d8eba4cdfa7b6a684b6016a099a83
beerfekt <christoph.beer@gmx.de>
2019-08-22 12:25:10 +0200

    dotnet run command fixed

--- setup.sh
+++ setup.sh
@@ -9,13 +9,15 @@ CONTAINERNAME=asp-net
 docker exec  $CONTAINERNAME dotnet new webApp -o /app/$APPNAME --no-https
 #only for development!!
 docker exec  $CONTAINERNAME chmod -R 777 /app 
-docker exec  $CONTAINERNAME cd /app/$APPNAME
-docker exec  $CONTAINERNAME dotnet run 
+#docker exec  $CONTAINERNAME dotnet run 
 docker exec  $CONTAINERNAME ln -s /etc/nginx/sites-available/docker-nginx.test /etc/nginx/sites-enabled/
 docker exec  $CONTAINERNAME ln -s /etc/nginx/sites-available/docker-asp.test /etc/nginx/sites-enabled/  
 docker exec  $CONTAINERNAME service nginx restart
+docker exec $CONTAINERNAME export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
 #hosteinträge setzen
 sudo sed -i "2i$IP $URLNGINX" /etc/hosts
 sudo sed -i "2i$IP $URLASP" /etc/hosts
+
+docker exec  $CONTAINERNAME dotnet run --project ./myWebApp/
 
